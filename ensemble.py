192ec4a0c890bc74b734a465dca1d7c57d044772
SpaJune <31369857+SpaJune@users.noreply.github.com>
2017-12-20 20:25:26 +0800

    更新

--- ensemble.py
+++ ensemble.py
@@ -1,5 +1,7 @@
 import pickle
-
+import numpy as np
+import math
+import random
 class AdaBoostClassifier:
     '''A simple AdaBoost Classifier.'''
 
@@ -10,6 +12,10 @@ class AdaBoostClassifier:
             weak_classifier: The class of weak classifier, which is recommend to be sklearn.tree.DecisionTreeClassifier.
             n_weakers_limit: The maximum number of weak classifier the model can use.
         '''
+        self.weak_classifier=weak_classifier
+        self.n_weakers_limit=n_weakers_limit
+        self.classifier=[]
+        self.weight_classifier=[]
         pass
 
     def is_good_enough(self):
@@ -23,6 +29,26 @@ class AdaBoostClassifier:
             X: An ndarray indicating the samples to be trained, which shape should be (n_samples,n_features).
             y: An ndarray indicating the ground-truth labels correspond to X, which shape should be (n_samples,1).
         '''
+        n_samples = X.shape[0]
+        n_features = X.shape[1]
+        w=np.ones(n_samples)
+        for i in range(self.n_weakers_limit):
+            classifier=self.weak_classifier(max_depth=3) #决策树的深度
+            classifier.fit(X,y,w)
+            score=1-classifier.score(X,y,w)   #加权错误率
+
+            if(score>0.5):
+                break
+            classifier_weight=0.5*math.log((1-score)/score)  #这里可以考虑加个很小的值，避免除以0的情况
+            self.weight_classifier.append(classifier_weight) #训练出来的权值加入列表
+            self.classifier.append(classifier)  #把训练出来的分类器加入列表
+
+            tmp=np.exp(-np.multiply(classifier.predict(X),np.transpose(y))*classifier_weight) #套公式，transpose是为了预测值与y值的维度变为一样（1xn变为nx1)
+            tmp=tmp/np.sum(tmp) #套公式
+            w=np.multiply(tmp,w).reshape([n_samples])
+
+
+            #np.multiply(np.multiply(classifier.predict(X)==y,-classifier_weight*np.ones(n_samples)),np.ones(n_samples))
         pass
 
 
@@ -35,6 +61,15 @@ class AdaBoostClassifier:
         Returns:
             An one-dimension ndarray indicating the scores of differnt samples, which shape should be (n_samples,1).
         '''
+        y=0
+        for i in range(self.n_weakers_limit):
+            if(i==0):
+                y=self.weight_classifier[i]*self.classifier[i].predict(X)
+            else:
+                y=np.vstack((y,self.weight_classifier[i]*self.classifier[i].predict(X)))
+        y=np.mean(y,0)  #加权平均
+        return y
+
         pass
 
     def predict(self, X, threshold=0):
@@ -47,6 +82,11 @@ class AdaBoostClassifier:
         Returns:
             An ndarray consists of predicted labels, which shape should be (n_samples,1).
         '''
+        y_=self.predict_scores(X)
+        y=(y_>=threshold)
+        return y
+
+
         pass
 
     @staticmethod
